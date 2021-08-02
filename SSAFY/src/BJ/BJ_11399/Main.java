78871b9c1ab9bcda4ca9126befc2a7a6b5b5c6f4
AhnHyeonho <ahndding@gmail.com>
2021-08-03 02:58:20 +0900

    20210803 BJ_11399 문제풀이

--- SSAFY/src/BJ/BJ_11399/Main.java
+++ SSAFY/src/BJ/BJ_11399/Main.java
@@ -1,4 +1,22 @@
 package BJ.BJ_11399;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+        int result = 0;
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        for (int i = 0; i < N; i++) {
+            result += arr[i] * (N - i);
+        }
+        System.out.println(result);
+    }
+
 }
