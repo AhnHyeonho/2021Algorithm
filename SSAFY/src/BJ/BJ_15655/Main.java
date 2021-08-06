7aa98edd3d9d998d1e0ce4bd67c6ed96a616d938
AhnHyeonho <ahndding@gmail.com>
2021-08-06 20:51:06 +0900

    20210806 BJ15655 문제풀이

---
+++ SSAFY/src/BJ/BJ_15655/Main.java
@@ -0,0 +1,38 @@
+package BJ.BJ_15655;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	static int N, M;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+		int[] arr = new int[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
+		}
+		Arrays.sort(arr);
+		combination(arr, new int[M], 0, 0);
+		System.out.println(sb.toString());
+		sc.close();
+	}
+
+	private static void combination(int[] arr, int[] sel, int idx, int k) {
+		if (k == sel.length) {
+			for (int i = 0; i < sel.length; i++) {
+				sb.append(sel[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+
+		for (int i = idx; i < arr.length; i++) {
+			sel[k] = arr[i];
+			combination(arr, sel, i + 1, k + 1);
+		}
+	}
+}
